4d9f9b2c01049fa09b30845de500105ab4104eb3
Eviscerating <tamakokitashirakawa666@gmail.com>
2025-02-17 12:35:01 +0000

    fixe

--- src/account/models.py
+++ src/account/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _t
+
 
 
 from .managers import CustomUserManager
@@ -10,9 +12,9 @@ FIRST_NAME_MAXLEN = 100
 LAST_NAME_MAXLEN = 100
 
 class CustomUser (AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True, verbose_name= 'email adress')
-    first_name= models.CharField(max_length=FIRST_NAME_MAXLEN)
-    last_name= models.CharField(max_length=LAST_NAME_MAXLEN)
+    email = models.EmailField(unique=True, verbose_name= _t('email adress'))
+    first_name= models.CharField(max_length=FIRST_NAME_MAXLEN, verbose_name= _t('first name'))
+    last_name= models.CharField(max_length=LAST_NAME_MAXLEN, verbose_name= _t('last adress'))
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
--- src/writer/views.py
+++ src/writer/views.py
@@ -4,8 +4,9 @@ from django.http import HttpRequest, HttpResponse, HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import aget_user
 
+from common.django_utils import arender
 from common.auth import awriter_required
 
 @awriter_required
 async def dashboard(request: HttpRequest) -> HttpResponse:
-            return render(request, 'writer/dashboard.html')
+            return await arender(request, 'writer/dashboard.html')
